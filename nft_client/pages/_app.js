b125da02c984758e5b43094929abb297d6da73e7
okste1234 <okste1234@gmail.com>
2024-02-11 16:53:54 +0100

    chore: add resuabble components - navbar et. footer, set up fontawesome

--- nft_client/pages/_app.js
+++ nft_client/pages/_app.js
@@ -1,5 +1,20 @@
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
+
+import { Navbar, Footer } from '../components';
 import '../styles/globals.css';
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="min-h-screen bg-white dark:bg-nft-dark">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+
+    <Script src="https://kit.fontawesome.com/536146d1a6.js" crossorigin="anonymous" />
+  </ThemeProvider>
+
+);
 
 export default MyApp;
