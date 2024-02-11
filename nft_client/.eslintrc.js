faa8f83ab429c3ae2363964720170ad6a631d85f
okste1234 <okste1234@gmail.com>
2024-02-11 19:59:43 +0100

    chore: add assets, implement navbar

--- nft_client/.eslintrc.js
+++ nft_client/.eslintrc.js
@@ -68,6 +68,8 @@ module.exports = {
     'object-curly-newline': 0,
     'react/jsx-filename-extension': 0,
     'react/jsx-indent': 0,
+    'react/jsx-indent-props': 0,
+    indent: 0,
     'react/jsx-one-expression-per-line': 0,
     'jsx-a11y/click-events-have-key-events': 0,
     'jsx-a11y/alt-text': 0,
